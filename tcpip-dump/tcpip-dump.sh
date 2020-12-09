6eee6649e286cf72e693925e2f6eb0de5131ccc0
UmedaTakefumi <ut+github@azurestone.net>
2020-12-09 22:34:36 +0900

    edit config

--- tcpip-dump/tcpip-dump.sh
+++ tcpip-dump/tcpip-dump.sh
@@ -2,8 +2,10 @@
 
 DATE_TIME=$(date +%Y-%m-%d_%H-%M-%S)
 SAVE_DIRPATH="/opt/tcpip-dump"
-LOTATION_SEC="60"
+LOTATION_SEC="3600"
 CAP_INTERFACE="enp0s8"
+
+
 
 if type "/usr/sbin/tshark" > /dev/null 2>&1; then
 
